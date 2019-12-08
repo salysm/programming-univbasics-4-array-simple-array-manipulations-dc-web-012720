0aeb0cc21f74777fdfa57edbb34a77f8a1775c45
Samantha Smith <smith.samantha.lynn@gmail.com>
2019-12-08 01:13:18 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -36,3 +36,5 @@ def using_concat(array)
   my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
     more_favs = ["sports cars", "flatiron school"]
     all_my_favs= array.concat(my_favorite_things, more_favs)
+    p all_my_favs
+  end
