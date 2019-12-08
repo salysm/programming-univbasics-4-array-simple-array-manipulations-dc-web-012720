fe81103ab13fd5e388e8b2853cd13c63f28c7575
Samantha Smith <smith.samantha.lynn@gmail.com>
2019-12-08 02:15:19 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -62,8 +62,8 @@ def using_insert(array, element)
    
    def using_delete(array, string)
      instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
-    no_offense_steven = instructors.delete("Steven")
-    p instructors
+    instructors.delete("Steven") = no_offense_steven
+    p no_offense_steven
   end
   
      
