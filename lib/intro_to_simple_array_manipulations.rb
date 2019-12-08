9b7c510fa4720624eef7c24c6e1cafc35798f061
Samantha Smith <smith.samantha.lynn@gmail.com>
2019-12-08 00:51:39 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -8,26 +8,11 @@ def using_unshift(array, element)
 bouroughs_in_nyc.unshift("Staten Island")
 end
 
-
-#describe "using_pop" do
- # before(:each) do 
-  #  @continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
-   # @deleted_string = using_pop(@continents)
-  #end
-  #it "takes in argument of an array and uses the pop method to remove the last element from the array and return that element" do 
-   # expect(@deleted_string).to eq("Antarctica")
-  #end
-
-  #it "decreases the length of the array by 1" do 
-   # expect(@continents.size).to eq(6)
-  #end
-#end
-
-
 def using_pop(array) 
   continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
   deleted_string = array.pop
   p deleted_string
 end
+
 
 
