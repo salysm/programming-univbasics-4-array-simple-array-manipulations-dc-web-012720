51efe6d68a4fa7d0c08270cdbb0d5f83f67a9eb5
Samantha Smith <smith.samantha.lynn@gmail.com>
2019-12-08 01:56:25 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -38,10 +38,10 @@ def using_concat(my_favorite_things, more_favs)
     more_favs = ["sports cars", "flatiron school"]
    all_my_favs = my_favorite_things.concat more_favs
    p all_my_favs
-   my_favorite_things.length 
   end
 
 
-def using_insert
+def using_insert(array, element)
   list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
-    @another_language = "Python"
+    another_language = "Python"
+    
