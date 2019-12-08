477834c3b32af19540e1688ccfe357a96fe5b1bf
Samantha Smith <smith.samantha.lynn@gmail.com>
2019-12-08 02:04:57 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -49,6 +49,7 @@ def using_insert(array, element)
   end
   
   def using_uniq(array)
-     it "takes in an argument of an array and uses the uniq method to remove any duplicate items" do
     haircuts = ["Pixie", "Bob", "Mohawk", "Crew Cut", "Linka", "Wheeler", "Bob"]
-    new_array = using_uniq(haircuts)
+    new_array = haircuts.uniq 
+    p new_array
+  end
