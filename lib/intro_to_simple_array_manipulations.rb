3f6b7e385bd177c2350d83e703e7f6c988ea4ce8
Samantha Smith <smith.samantha.lynn@gmail.com>
2019-12-08 01:58:42 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -44,4 +44,6 @@ def using_concat(my_favorite_things, more_favs)
 def using_insert(array, element)
   list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
     another_language = "Python"
-    
+    array.insert(another_language)
+    p list_of_programming_languages
+  end
