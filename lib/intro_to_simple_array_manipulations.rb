bc6b649afcbfc6d1caf677726469c9967f593ff6
Samantha Smith <smith.samantha.lynn@gmail.com>
2019-12-08 00:53:49 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -15,6 +15,9 @@ def using_pop(array)
 end
 
 def pop_with_args(array)
-  
+  dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
+  small_dogs = array.pop(2)
+  p small_dogs
+end 
 
 
